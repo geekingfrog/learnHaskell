b65af637ca173044b63a1e5713bc172020fbdf6c
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2015-03-28 11:38:35 +0000

    Parser for valid ping lines

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import System.Environment
+import Data.List
+-- import System.IO
+-- import qualified Data.ByteString.Lazy as BL
+
+getFileName :: [String] -> String
+getFileName [] = "./googlePing.dat"
+getFileName (file:_) = file
+
+-- Some lines have a different format like below
+-- 64 bytes from 216.58.210.46: icmp_seq=133 ttl=58 time=115 ms
+-- These are going to be ignored (at least in the first version of this
+-- toy program)
+isValidPing :: String -> Bool
+isValidPing line | "64 bytes" `isPrefixOf` line = True
+                 | otherwise = False
+
+-- preprocessPingData :: ByteString -> [String]
+-- pre
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  args <- getArgs
+  let filePath = getFileName args
+  putStrLn ("going to process " ++ filePath)
+  rawPingData <- readFile filePath
+  let pingData = filter isValidPing $ lines rawPingData
+  putStrLn ((show $ length pingData) ++ "lines to process")
---
+++ src/parser.hs
@@ -0,0 +1,59 @@
+module PingParser (parsePingFile, Ping) where
+
+import Text.ParserCombinators.Parsec
+import Text.Parsec.Char (endOfLine)
+import Numeric (readFloat)
+import Control.Applicative (empty, (<$), (<$>), (<*>), (<*))
+import Data.Char (digitToInt)
+import Data.List (foldl')
+
+-- utils to parse int and float
+positiveFloat :: CharParser () Float
+positiveFloat = do
+  s <- getInput
+  case (readFloat s) of
+    [(n, s')] -> n <$ setInput s'
+    _         -> empty
+
+positiveNatural :: CharParser () Int
+positiveNatural =
+  foldl' (\a i -> a * 10 + digitToInt i) 0 <$> many1 digit
+
+data Ping = Ping {icmpSeq :: Int, time :: Float} deriving (Show)
+
+pingLine :: CharParser () (Maybe Ping)
+pingLine = pingHeader <|> (host >> pingData)
+
+pingHeader :: CharParser () (Maybe Ping)
+pingHeader = (string "PING" >> (many $ noneOf "\n")) >> return Nothing
+
+host :: CharParser () String
+host = many (noneOf ":") <* char ':' <* spaces
+
+-- fancy applicative version
+pingData :: CharParser () (Maybe Ping)
+pingData = Just <$> (
+    Ping <$> (pair positiveNatural <* spaces)
+    -- discard the ttl and the remaining " ms" at the end of the line
+    <*> between (pair positiveNatural) (string " ms") (pair positiveFloat)
+  )
+
+-- -- same as above with a do notation
+-- pingData = do
+--   icmp <- pair positiveNatural
+--   spaces
+--   _ <- pair positiveNatural -- ttl
+--   spaces
+--   time <- pair positiveFloat
+--   spaces >>  string "ms"
+--   return Ping {icmpSeq=icmp, time=time}
+
+
+pair :: CharParser () a -> CharParser () a
+pair parser = (many (noneOf "=")) >> (char '=') >> parser
+
+pingFile :: CharParser () [Maybe Ping]
+pingFile = sepEndBy pingLine endOfLine
+
+parsePingFile :: String -> Either ParseError [Maybe Ping]
+parsePingFile input = parse pingFile "(unknown)" input
